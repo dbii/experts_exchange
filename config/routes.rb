fc0673f89308aadbb3d097debf5444e64b26053f
David Bluestein II <dbii@interaction.net>
2018-08-29 14:26:46 -0500

    Added Expert Controller routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,7 @@
 Rails.application.routes.draw do
-  get 'experts/index'
-  get 'experts/show'
-  get 'experts/new'
-  get 'experts/create'
-  get 'experts/edit'
-  get 'experts/update'
-  root to: 'pages#about'
+
+  resources :experts
+  root to: 'experts#index'
 
   get '/logout',  to: 'user_sessions#destroy', as: :logout
   get '/login',   to: 'user_sessions#new',     as: :login
