0d3ad5c2eea9191c7e759ced2b4c4325f1d5792e
Steven Rogers <srogers1@gmail.com>
2018-08-29 14:37:58 -0500

    Add some simple navigation

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
+  root to: 'pages#about'
 
   resources :experts
-  root to: 'experts#index'
 
   get '/logout',  to: 'user_sessions#destroy', as: :logout
   get '/login',   to: 'user_sessions#new',     as: :login
