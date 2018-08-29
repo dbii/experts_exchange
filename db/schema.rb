0c0ffe902947e0328b720d458fee70c058ddd75b
David Bluestein II <dbii@interaction.net>
2018-08-29 11:41:42 -0500

    Added schema updates

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,26 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_08_29_160835) do
+ActiveRecord::Schema.define(version: 2018_08_29_163828) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "experts", force: :cascade do |t|
+    t.string "name"
+    t.string "url"
+    t.string "short_url"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "topics", force: :cascade do |t|
+    t.integer "expert_id"
+    t.string "tag"
+    t.string "content"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "email"
